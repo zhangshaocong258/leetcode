f6261e718268879289c03b6d0b55d6648de8f82d
zhangshaocong258 <597001494@qq.com>
2016-03-01 18:59:22 +0800

    完整ContainsDuplicate

---
+++ src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * Created by zsc on 2016/3/1.
+ */
+class Solution217{
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set= new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
+public class ContainsDuplicate {
+    public static void main(String[] args ){
+        int[] num = {1,2};
+        boolean result = new Solution217().containsDuplicate(num);
+        System.out.println(result);
+    }
+}

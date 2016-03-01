915fb43905740c685f73dde2abf67d6aa4b0b541
zhangshaocong258 <597001494@qq.com>
2016-03-01 20:57:07 +0800

    BinaryTreePreorderTraversal and BinaryTreePostorderTraversal

---
+++ src/leetcode/BinaryTreePostorderTraversal.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zsc on 2016/3/1.
+ */
+class Solution145 {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null)
+            return list;
+        helper(root, list);
+        return list;
+    }
+
+    public void helper(TreeNode root, ArrayList<Integer> list) {
+        if (root != null) {
+            helper(root.left, list);
+            helper(root.right, list);
+            list.add(root.val);
+        }
+    }
+}
+
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        TreeNode Tree145 = new TreeNode(8);
+        Tree145.left = new TreeNode(2);
+        Tree145.right = new TreeNode(3);
+        Tree145.left.left = new TreeNode(4);
+        List result = new Solution145().postorderTraversal(Tree145);
+        System.out.println(result);
+    }
+}
---
+++ src/leetcode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zsc on 2016/3/1.
+ */
+class Solution144 {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null)
+            return list;
+        helper(root, list);
+        return list;
+    }
+
+    public void helper(TreeNode root, ArrayList<Integer> list) {
+        if (root != null) {
+            list.add(root.val);
+            helper(root.left, list);
+            helper(root.right, list);
+        }
+    }
+}
+
+public class BinaryTreePreorderTraversal {
+    public static void main(String[] args) {
+        TreeNode Tree144 = new TreeNode(6);
+        Tree144.left = new TreeNode(2);
+        Tree144.right = new TreeNode(3);
+        Tree144.left.left = new TreeNode(4);
+        List result = new Solution144().preorderTraversal(Tree144);
+        System.out.println(result);
+    }
+}
+
+

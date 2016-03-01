eaf0da1ac6c4f1862031a6b9ebe62b6db39a0bde
zhangshaocong258 <597001494@qq.com>
2016-03-01 20:45:36 +0800

    inorderTraversal

---
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import javafx.collections.transformation.SortedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zsc on 2016/3/1.
+ */
+class Solution99 {
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null)
+            return list;
+        helper(root, list);
+        return list;
+    }
+
+    public void helper(TreeNode root, ArrayList<Integer> list) {
+        if (root != null) {
+            helper(root.left, list);
+            list.add(root.val);
+            helper(root.right, list);
+        }
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        TreeNode Tree99 = new TreeNode(1);
+        Tree99.left = new TreeNode(2);
+        Tree99.right = new TreeNode(3);
+        Tree99.left.left = new TreeNode(4);
+        List result = new Solution99().inorderTraversal(Tree99);
+        System.out.println(result);
+    }
+}

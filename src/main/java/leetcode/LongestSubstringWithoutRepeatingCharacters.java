86d0d37761a327930238dbca058b1aee78024bac
zhangshaocong258 <597001494@qq.com>
2017-01-31 16:46:04 +0800

    2个

---
+++ src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * Created by zsc on 2017/1/30.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String args[]) {
+        String str = "";
+        System.out.println(new solution3().lengthOfLongestSubstring(str));
+    }
+
+}
+
+class solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        int result = 0;
+        int i = 0;
+        int j = 0;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        while (j < s.length()) {
+            if (!map.containsKey(String.valueOf(s.charAt(j)))) {
+                map.put(String.valueOf(s.charAt(j)), j);
+            } else {
+                int length = j - i;
+                if (result < length) {
+                    result = length;
+                }
+                int index = map.get(String.valueOf(s.charAt(j)));
+                i = Math.max(i, index + 1);
+                map.put(String.valueOf(s.charAt(j)), j);
+            }
+            j++;
+        }
+        if (result < j - i) {
+            result = j - i;
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/leetcode/TwoSum.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * Created by zsc on 2017/1/30.
+ */
+public class TwoSum {
+    public static void main(String args[]) {
+        solution1 solution1 = new solution1();
+        int[] a = {0, 4, 3, 0};
+        for (int i = 0; i < solution1.twoSum(a, 0).length; i++) {
+            System.out.println(solution1.twoSum(a, 0)[i]);
+
+        }
+    }
+}
+
+class solution1{
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
